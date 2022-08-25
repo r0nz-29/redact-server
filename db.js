d24da7a19af4e957b2d9e44c69747f5f6e192253
r0nz-29 <gs2019048@sgsitsindore.in>
2022-08-25 23:40:29 +0530

    updated db config

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 import pg from "pg";
 import "dotenv/config";
 
-const connectionString = process.env.DATABASE_URL+"?sslmode=require";
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new pg.Pool({
-	connectionString: connectionString
-	// ssl: {
-	// 	rejectUnauthorized: false
-	// }
+	connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
-
-console.log(connectionString);
 
 export default pool;
